3c0ad20996c02c326a48a137c5df619d807f601c
Steven Wright <sewright22@gmail.com>
2024-04-21 10:53:28 -0500

    Created projects for data.

---
+++ TypeOneFoodJournal.Services/IInsulinPumpService.cs
@@ -0,0 +1,12 @@
+﻿namespace TypeOneFoodJournal.Services
+{
+    using TypeOneFoodJournal.Data;
+
+    public interface IInsulinPumpService
+    {
+        string Token { get; }
+        Task Login(string username, string password);
+        void UpdateToken(string token, string userId);
+        Task<ReadingList> GetInsulinPumpDataAsync(DateTime startDate, DateTime endDate);
+    }
+}
